44460c3a3e752ffb3550c86e6e826a1b844b5df7
Mueid <mrmuyed579@gmail.com>
2022-05-22 02:53:38 +0600

    new arrival section done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../Share/Footer';
 import Banner from './Banner';
 import BusinessSummery from './BusinessSummery';
+import NewArrival from './NewArrival';
 import Parts from './Parts';
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <Banner></Banner>
             <Parts></Parts>
             <BusinessSummery></BusinessSummery>
+            <NewArrival></NewArrival>
             <Footer></Footer>
         </div>
     );
