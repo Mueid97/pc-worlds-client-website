c1a5756f97d69c02a6bdaa03b2a02e2f479ec69d
Mueid <mrmuyed579@gmail.com>
2022-05-22 20:32:39 +0600

    data load form server

--- src/pages/Home/Parts.js
+++ src/pages/Home/Parts.js
@@ -4,7 +4,7 @@ import Part from './Part';
 const Parts = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('products.json')
+        fetch('http://localhost:5000/product')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
@@ -13,7 +13,7 @@ const Parts = () => {
             <h1 className='text-center font-extrabold text-4xl font-serif text-black mt-10 uppercase'>All Parts</h1>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-14 mt-10'>
                 {
-                    products.map(product => <Part key={product.id} product={product}></Part>)
+                    products.map(product => <Part key={product._id} product={product}></Part>)
                 }
             </div>
         </div>
