a7ffc44fe4a40279fbfe749a0c1f48e9f9d84c3f
Mueid <mrmuyed579@gmail.com>
2022-06-11 23:05:15 +0600

    add animation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,16 +6,22 @@ import BusinessSummery from './BusinessSummery';
 import NewArrival from './NewArrival';
 import Parts from './Parts';
 import Vision from './Vision';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 const Home = () => {
+    useEffect(()=>{
+        AOS.init({duration: 2000});
+    },[])
     return (
         <div>
             <Banner></Banner>
-            <Parts></Parts>
-            <NewArrival></NewArrival>
-            <Vision></Vision>
-            <BusinessSummery></BusinessSummery>
-            <Reviews></Reviews>
+           <div data-aos="fade-right"><Parts></Parts></div>
+            <div data-aos="fade-down"><NewArrival></NewArrival></div>
+            <div data-aos="fade-up"><Vision></Vision></div>
+            <div data-aos="fade-down"><BusinessSummery></BusinessSummery></div>
+            <div data-aos="fade-left"><Reviews></Reviews></div>
             <Footer></Footer>
         </div>
     );
